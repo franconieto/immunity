4f04d085dd107f27e322e3f9d971d55d26744621
lmayorga <lmayorga@fcm.uncu.edu.ar>
2018-12-13 14:17:06 -0300

    cambios en tether (más frecuente para Golgi)

--- src/immunity/MT.java
+++ src/immunity/MT.java
@@ -36,7 +36,7 @@ public class MT {
 	public void changePosition(MT mt) {
 		//if (Math.random() < 0.1) return;
 		// move the origin and the end of the MT
-		xorigin = RandomHelper.nextDoubleFromTo(15, 35);
+		xorigin = RandomHelper.nextDoubleFromTo(10, 40);
 		if (xorigin <= 25) {xend = xorigin -RandomHelper.nextDoubleFromTo(0, xorigin);}
 		else {xend = xorigin + RandomHelper.nextDoubleFromTo(0, 50-xorigin);}
 		double mth = Math.atan((50) / (xend - xorigin));
